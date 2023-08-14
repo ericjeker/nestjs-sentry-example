bcffdaada752baed3eb5bed2d5ad4ee521da5d29
Eric Jeker <eric@wowww.ch>
2023-08-14 18:26:05 +0200

    feat: remove request scoped

--- src/sentry/sentry.interceptor.ts
+++ src/sentry/sentry.interceptor.ts
@@ -3,37 +3,32 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
-  Scope,
 } from '@nestjs/common';
 import { catchError, finalize, Observable, throwError } from 'rxjs';
 import { SentryService } from './sentry.service';
 import * as Sentry from '@sentry/node';
 
-/**
- * We must be in Request scope as we inject SentryService
- */
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class SentryInterceptor implements NestInterceptor {
   constructor(private sentryService: SentryService) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // start a child span for performance tracing
-    const span = this.sentryService.startChild({ op: `route handler` });
+    const request = context.switchToHttp().getRequest();
+
+    const span = this.sentryService.getRequestSpan(request, {
+      op: `Route Handler`,
+    });
 
     return next.handle().pipe(
       catchError((error) => {
         // capture the error, you can filter out some errors here
-        Sentry.captureException(
-          error,
-          this.sentryService.span.getTraceContext(),
-        );
+        Sentry.captureException(error, span.getTraceContext());
 
         // throw again the error
         return throwError(() => error);
       }),
       finalize(() => {
         span.finish();
-        this.sentryService.span.finish();
       }),
     );
   }
--- src/sentry/sentry.service.ts
+++ src/sentry/sentry.service.ts
@@ -1,14 +1,9 @@
 import { Request } from 'express';
-import { Scope } from '@nestjs/common';
-import { Inject, Injectable } from '@nestjs/common';
-import { REQUEST } from '@nestjs/core';
+import { Injectable } from '@nestjs/common';
 import * as Sentry from '@sentry/node';
 import { Span, SpanContext } from '@sentry/types';
 
-/**
- * Because we inject REQUEST we need to set the service as request scoped
- */
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class SentryService {
   /**
    * Return the current span defined in the current Hub and Scope
@@ -17,31 +12,29 @@ export class SentryService {
     return Sentry.getCurrentHub().getScope().getSpan();
   }
 
-  /**
-   * When injecting the service it will create the main transaction
-   *
-   * @param request
-   */
-  constructor(@Inject(REQUEST) private request: Request) {
-    const { method, headers, url } = this.request;
+  public getRequestSpan(request: Request, spanContext: SpanContext) {
+    const { method, headers, url } = request;
 
-    // recreate transaction based from HTTP request
     const transaction = Sentry.startTransaction({
       name: `Route: ${method} ${url}`,
       op: 'transaction',
     });
 
-    // setup context of newly created transaction
     Sentry.getCurrentHub().configureScope((scope) => {
       scope.setSpan(transaction);
 
-      // customize your context here
       scope.setContext('http', {
         method,
         url,
         headers,
       });
     });
+
+    const span = Sentry.getCurrentHub().getScope().getSpan();
+
+    span.startChild(spanContext);
+
+    return span;
   }
 
   /**
